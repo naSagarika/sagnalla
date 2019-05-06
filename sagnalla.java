9baad0c78cf0b7b39bdd1dcf23fceea0298f0751
naSagarika <42429289+naSagarika@users.noreply.github.com>
2019-05-06 11:21:06 +0530

    sagnalla.java

--- sagnalla.java
+++ sagnalla.java
@@ -4,9 +4,9 @@ public static void main(string args[])
 {
 for (int i=1;i<=5;i++)
 {
-for(int j=1:j<=5;j++)
+for(int j=1;j<=5;j++)
 {
-system.out.print("*"):
+system.out.print("*");
 }
 system.out.println();
 }
